02f927b5464effa27c2efd4e772b5a36309968f8
Prateek Varma <prtk.varma@gmail.com>
2020-12-14 18:18:06 -0600

    update a planet

--- app.py
+++ app.py
@@ -168,7 +168,9 @@ def planets_details(planet_id: int):
         return jsonify(message="No planets found"), 404
 
 
+# Remember, you can get the access token, returned from login
 @app.route('/add-planet', methods=['POST'])
+@jwt_required # just this line does the JWT check
 def add_planet():
     planet_name = request.form['planet_name']
     test = Planet.query.filter_by(planet_name=planet_name).first()
@@ -179,9 +181,9 @@ def add_planet():
         # planet_name = request.form['planet_name']
         planet_type = request.form['planet_type']
         home_star = request.form['home_star']
-        mass = request.form['mass']
-        radius = request.form['radius']
-        distance = request.form['distance']
+        mass = float(request.form['mass'])
+        radius = float(request.form['radius'])
+        distance = float(request.form['distance'])
 
         # SQLAlchemy
         new_planet = Planet(planet_name=planet_name,
@@ -194,6 +196,24 @@ def add_planet():
         db.session.add(new_planet)
         db.session.commit()
         return jsonify(message='You added a planet!'), 201
+
+
+@app.route('/update-planet', methods=['PUT'])
+@jwt_required # this is all that's required to do a JWT check
+def update_planet():
+    planet_id = int(request.form['planet_id'])
+    planet = Planet.query.filter_by(planet_id=planet_id).first()
+    if planet:
+        planet.planet_name = request.form['planet_name']
+        planet.planet_type = request.form['planet_type']
+        planet.home_star = request.form['home_star']
+        planet.mass = float(request.form['mass'])
+        planet.radius = float(request.form['radius'])
+        planet.distance = float(request.form['distance'])
+        db.session.commit()
+        return jsonify(message="Planet updated"), 202
+    else:
+        return jsonify(message="No planet found"), 404
 
 
 if __name__ == '__main__':
